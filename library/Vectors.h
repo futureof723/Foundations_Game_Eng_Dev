3eab6f0f33a6a0291594e20309e6226aa8784ea8
futur <futureof723@gmail.com>
2025-03-04 01:54:27 -0500

    Refactored functions related to manipulating vectors. Added in a text file with notes related to vectors and operations on vectors.

--- library/Vectors.h
+++ library/Vectors.h
@@ -5,8 +5,6 @@
 #ifndef VECTORS_H
 #define VECTORS_H
 
-
-
 struct Vector3D {
     float x, y, z;
 
@@ -17,12 +15,30 @@ struct Vector3D {
     float& operator[](int i);
 
     const float& operator[](int i) const;
+
+    Vector3D& operator *=(float s);
+    Vector3D& operator /=(float s);
 };
 
-float find_magnitude(Vector3D& a);
+// Scalar multiplication
+inline Vector3D operator *(const Vector3D& v, float s);
+
+// Scalar division (in-place)
+inline Vector3D operator /(const Vector3D& v, float s);
+
+// Negating the vector
+inline Vector3D operator -(const Vector3D& v);
+
+// Function to compute the magnitude of a vector
+inline float Magnitude(const Vector3D& v);
+
+// Normalize function
+inline Vector3D Normalize(const Vector3D& v);
+
+// Finding the distance between 2 vectors
 float distance_3D(Vector3D& a, Vector3D& b);
+
+// changing the magnitude of a vector
 void change_magnitude(Vector3D& a, float scalar_value);
-
-
 
 #endif //VECTORS_H
--- main.cpp
+++ main.cpp
@@ -11,15 +11,5 @@ int main() {
     std::cout << "Vector v2 using constructor: " << std::endl;
     std::cout << v2[0] << v2[1] << v2[2] << std::endl;
 
-    std::cout << "Magnitude of Vector v: " << find_magnitude(v) << std::endl;
-    std::cout << "Magnitude of Vector v2: " << find_magnitude(v2) << std::endl;
-
-    std::cout << "Distance between v and v2: " << distance_3D(v, v2) << std::endl;
-
-    std::cout << "Changing the magnitude of v2..." << std::endl;
-    change_magnitude(v2, 2.0f);
-    std::cout << "New magnitude of v2: " << find_magnitude(v2) << std::endl;
-    std::cout << "New distance between v and v2: " << distance_3D(v, v2) << std::endl;
-
     return 0;
 }
